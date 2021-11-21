12385b95747ba30f59570834e382ce5b18a15582
Jaime Andrés Monsalve Ballesteros <70330970+andresmonsalve19@users.noreply.github.com>
2021-11-21 16:51:58 -0500

    tercer intento

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -9,12 +9,15 @@ public class Mamifero extends Animal {
 	private boolean pelaje;
 	private int patas;
 	
-	public Mamifero() {}
+	public Mamifero() {
+		listado.add(this);
+	}
 	
 	public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
 		super(nombre, edad, habitat, genero);
 		this.pelaje = pelaje;
 		this.patas = patas;
+		listado.add(this);
 	}
 
 	public static int cantidadMamiferos() {
